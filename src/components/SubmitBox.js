ddfa78b68ff50566ba42c74d39707aa3f6714a70
dongminlee <gmgmgun@gmail.com>
2024-03-26 13:04:06 +0900

    README 수정

--- src/components/SubmitBox.js
+++ src/components/SubmitBox.js
@@ -1,11 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import "react-toastify/dist/ReactToastify.css";
-import contact from "../assets/images/contact.png";
+import React from 'react';
+import styled from 'styled-components';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SubmitBox = ({
   contactInput,
-  onChangeInput,
   onSubmit,
   stagedFileList,
   isAllSelected,
@@ -17,28 +15,15 @@ const SubmitBox = ({
 
   //업로드 버튼 클릭시 텍스트를 바꿔주는 함수. 업로드 버튼을 누르면 취소 버튼으로 바뀌고, 취소 버튼을 누르면 업로드 버튼으로 바뀐다. 업로드 실패 시 처리 추가 구현해야 함.
   const handleBtnChange = (e) => {
-    e.target.name === "submit" ? setIsAbort(true) : setIsAbort(false);
+    e.target.name === 'submit' ? setIsAbort(true) : setIsAbort(false);
   };
 
   //추가 구현 필요: 업로드 버튼 활성화 조건. 현재 isAllSelected가 단일 변수이기 때문에 리스트로 관리하도록 변경해야 제대로 작동함.
   const onCheck =
-    (stagedFileList && stagedFileList.length) !== 0 &&
-    contactInput.length === 11 &&
-    isAllSelected;
+    (stagedFileList && stagedFileList.length) !== 0 && isAllSelected;
 
   return (
     <SubmitWrap>
-      <Contact>
-        <ContactIcon src={contact} alt="contact icon" />
-        <ContactTitle>전화번호</ContactTitle>
-        <ContactInput
-          type="text"
-          value={contactInput}
-          onChange={onChangeInput}
-          maxLength="11"
-        ></ContactInput>
-        <ContactSample>하이폰(-)을 빼고 입력해주세요.</ContactSample>
-      </Contact>
       {!isAbort ? (
         <SubmitButton
           name="submit"
@@ -68,7 +53,7 @@ const SubmitBox = ({
 
 //하단 (연락처입력, 업로드버튼)
 const SubmitWrap = styled.div`
-  ${({theme}) => theme.mixin.flex("flex", "space-between", "center")};
+  ${({ theme }) => theme.mixin.flex('flex', 'flex-end', 'center')};
   width: 100%;
   max-width: 700px;
   min-width: 300px;
@@ -77,69 +62,6 @@ const SubmitWrap = styled.div`
   @media (max-width: 650px) {
     display: grid;
     justify-content: stretch;
-  }
-`;
-
-const Contact = styled.div`
-  ${({theme}) => theme.mixin.flex("flex", "space-flex-start", "center")};
-  width: 100%;
-  position: relative;
-  @media (max-width: 650px) {
-    margin-bottom: 10px;
-    width: 100%;
-    padding: 0px 0px 10px 0px;
-    border-bottom: 1px solid #dcdcdc;
-  }
-`;
-const ContactIcon = styled.img`
-  padding: 0px 10px 5px 15px;
-  width: 40px;
-  @media (max-width: 650px) {
-    width: 20px;
-  }
-`;
-const ContactTitle = styled.div`
-  margin-left: 11px;
-  font-weight: 700;
-  font-size: 17px;
-  line-height: 10px;
-  @media (max-width: 650px) {
-    font-size: 14px;
-  }
-`;
-
-const ContactSample = styled.div`
-  font-size: 14px;
-  @media (max-width: 500px) {
-    position: absolute;
-    right: 1%;
-    bottom: 55%;
-    font-size: 10px;
-  }
-`;
-
-const ContactInput = styled.input`
-  height: 30px;
-  width: 140px;
-  margin: 0px 10px;
-  padding: 0px 20px;
-  background-color: #ffffff;
-  border: 1px solid #dcdcdc;
-  border-radius: 20px;
-  text-align: center;
-  font-size: 17px;
-  font-weight: 550;
-  text-align: left;
-
-  cursor: pointer;
-  :focus {
-    outline: none;
-  }
-  @media (max-width: 400px) {
-    font-size: 14px;
-    width: 90px;
-    height: 26px;
-    padding: 0px 9px;
   }
 `;
 
